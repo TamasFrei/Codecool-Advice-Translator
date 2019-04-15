29765d9a108771463ad37450611ff0ddefebdfb9
cortinadampezzo <kisgans@gmail.com>
2019-04-15 13:31:54 +0200

    start project

---
+++ advice-translator/src/main/java/com/codecool/advicetranslator/AdviceTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.advicetranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceTranslatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdviceTranslatorApplication.class, args);
+    }
+
+}
---
+++ background-image/src/main/java/com/codecool/backgroundimage/BackgroundImageApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.backgroundimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgroundImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackgroundImageApplication.class, args);
+    }
+
+}
