0488d3076ba138d215ed0e502c4b1a03bfbff722
Tamas Frei <frei.tamas555@gmail.com>
2019-04-16 13:58:26 +0200

    Add Slf4j

--- advice/src/main/java/com/codecool/advice/service/AdviceService.java
+++ advice/src/main/java/com/codecool/advice/service/AdviceService.java
@@ -1,9 +1,11 @@
 package com.codecool.advice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class AdviceService {
 
     @Value("${advice.url}")
