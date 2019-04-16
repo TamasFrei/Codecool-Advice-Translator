3c7c7dfc5bbe6a2bb632af813fef2dbc2ca9a074
Tamas Frei <frei.tamas555@gmail.com>
2019-04-16 13:56:20 +0200

    Add external API url

--- advice/src/main/java/com/codecool/advice/service/AdviceService.java
+++ advice/src/main/java/com/codecool/advice/service/AdviceService.java
@@ -1,7 +1,11 @@
 package com.codecool.advice.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdviceService {
+
+    @Value("${advice.url}")
+    private String url;
 }
