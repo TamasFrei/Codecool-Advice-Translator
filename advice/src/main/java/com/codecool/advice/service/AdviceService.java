a6b17d68f7a0ef4ab32f3a0b3e9358f7ef279a50
Tamas Frei <frei.tamas555@gmail.com>
2019-04-16 13:59:50 +0200

    Inject RestTemplate

--- advice/src/main/java/com/codecool/advice/service/AdviceService.java
+++ advice/src/main/java/com/codecool/advice/service/AdviceService.java
@@ -1,12 +1,17 @@
 package com.codecool.advice.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 public class AdviceService {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Value("${advice.url}")
     private String url;
