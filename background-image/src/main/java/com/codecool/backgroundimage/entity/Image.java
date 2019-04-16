d474354d29f8f6ac0bd8ccc7e56f0b61a7671ad6
kemenykristof <kemeny.kristof24@gmail.com>
2019-04-16 13:48:53 +0200

    added lombok annotations for image

--- background-image/src/main/java/com/codecool/backgroundimage/entity/Image.java
+++ background-image/src/main/java/com/codecool/backgroundimage/entity/Image.java
@@ -1,10 +1,19 @@
 package com.codecool.backgroundimage.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Image {
 
     @Id
--- background-image/src/main/java/com/codecool/backgroundimage/repository/ImageRepository.java
+++ background-image/src/main/java/com/codecool/backgroundimage/repository/ImageRepository.java
@@ -1,4 +1,4 @@
-package com.codecool.backgroundimage;
+package com.codecool.backgroundimage.repository;
 
 import com.codecool.backgroundimage.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
