dff47a72dd555cf8151b918be0988b27755f08f4
Csilla Urban <csillaurban@gmail.com>
2019-04-16 14:16:50 +0200

    resttemplate bean to main

--- translator/src/main/java/com/codecool/translator/TranslatorApplication.java
+++ translator/src/main/java/com/codecool/translator/TranslatorApplication.java
@@ -1,13 +1,23 @@
 package com.codecool.translator;
 
+import org.hibernate.validator.constraints.EAN;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TranslatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TranslatorApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
