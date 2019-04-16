ca9c03bc06ad8f17d364bec085501411cec7896d
Tamas Frei <frei.tamas555@gmail.com>
2019-04-16 14:12:38 +0200

    Inject AdviceService

--- advice/src/main/java/com/codecool/advice/controller/AdviceController.java
+++ advice/src/main/java/com/codecool/advice/controller/AdviceController.java
@@ -1,9 +1,14 @@
 package com.codecool.advice.controller;
 
+import com.codecool.advice.service.AdviceService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class AdviceController {
+
+    @Autowired
+    private AdviceService adviceService;
 }
