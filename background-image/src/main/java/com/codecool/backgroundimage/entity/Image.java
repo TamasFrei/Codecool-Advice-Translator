bccf006b0aa6ac2c417265c2d1543fc28b0f5647
kemenykristof <kemeny.kristof24@gmail.com>
2019-04-17 12:58:27 +0200

    Merge branch 'development' of /home/kr24/IdeaProjects/advice-translator with conflicts.

--- background-image/src/main/java/com/codecool/backgroundimage/entity/Image.java
+++ background-image/src/main/java/com/codecool/backgroundimage/entity/Image.java
@@ -1,19 +1,27 @@
 package com.codecool.backgroundimage.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Image {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
 
-    public Image(String name){
-        this.name = name;
-    }
+    @Enumerated(value = EnumType.STRING)
+    private Language language;
+
+
 }
