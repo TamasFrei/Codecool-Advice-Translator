2171de4369b3f7f096c6babb25af6e5bc030406f
Csilla Urban <csillaurban@gmail.com>
2019-04-17 16:28:37 +0200

    call image controller

--- advice-translator/src/main/java/com/codecool/advicetranslator/service/ImageServiceCaller.java
+++ advice-translator/src/main/java/com/codecool/advicetranslator/service/ImageServiceCaller.java
@@ -1,8 +1,8 @@
 package com.codecool.advicetranslator.service;
 
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,15 +10,15 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class ImageServiceCaller {
 
-//    @Value("${}")
-//    private String baseUrl;
+    @Value("${image.url}")
+    private String baseUrl;
 
     @Autowired
     private RestTemplate restTemplate;
 
     public String getImageForLanguage(String language) {
-        //String image = restTemplate.getForEntity("imagecontoroller", String.class).getBody();
-        return "image";
+        String image = restTemplate.getForEntity(baseUrl + language, String.class).getBody();
+        return image;
     }
 
 }
