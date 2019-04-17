99b7faa909a254349dee6c750e69fdb2d7eeae59
Tamas Frei <frei.tamas555@gmail.com>
2019-04-17 19:51:44 +0200

    Add LoadBalanced to restTemplate

--- advice-translator/src/main/java/com/codecool/advicetranslator/AdviceTranslatorApplication.java
+++ advice-translator/src/main/java/com/codecool/advicetranslator/AdviceTranslatorApplication.java
@@ -2,6 +2,7 @@ package com.codecool.advicetranslator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -30,6 +31,7 @@ public class AdviceTranslatorApplication {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
